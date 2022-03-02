ea1a3a16089f3a4da0a0fef3b7457b232ef69c30
buniChristian <98418329+buniChristian@users.noreply.github.com>
2022-03-01 22:35:20 -0500

    Added names and Date

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -3,7 +3,7 @@
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
  * @author Christian Buni and Al Justin Pinto
- * March 1, 2022
+ * March 1, 2022 
  */
 package ca.sheridancollege.project;
 
